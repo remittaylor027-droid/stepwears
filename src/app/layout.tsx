61732328801c35d72e014bd9f1d1025aa4c51aa8
remittaylor027 <remittaylor027@gmail.com>
2026-06-08 15:26:07 +0500

    Add Google AdSense script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,6 +35,13 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
+      <head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8467889658250907"
+          crossOrigin="anonymous"
+        ></script>
+      </head>
       <body className="min-h-full flex flex-col pt-16">
         <CartProvider>
           <Navbar />
